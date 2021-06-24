7829f03457b979c19d1d68f7c1f3d8979b80f0d0
Daniel Espinosa <d111092.de@gmail.com>
2021-06-24 00:42:24 -0500

    style: fix ranking screen position

--- src/components/RankingScreen/RankingScreen.tsx
+++ src/components/RankingScreen/RankingScreen.tsx
@@ -15,20 +15,22 @@ const RankingScreen = () => {
 
     return (
         <div className={styles.rankingScreen}>
-            <Link className={styles.link} to="/home">
-                <Button
-                    label='Home'
-                    color='#4CC0A6'
-                />
-            </Link>
-            <h2>Ranking</h2>
+            <div className={styles.buttons}>
+                <Link className={styles.link} to="/home">
+                    <Button
+                        label='Home'
+                        color='#4CC0A6'
+                    />
+                </Link>
+            </div>
             <ul className={styles.ranking}>
                 {
                     ranking.sort((a: { time: number; }, b: { time: number; }) => (a.time - b.time)).slice(0, 4).map((player: { nickname: string; time: number; }, idx: number) => {
                         return (
+                            // TODO: Create a card component
                             <li key={idx}>
                                 {idx + 1} - {player.nickname}
-                                <p>{`Time: ${player.time}`}</p>
+                                <p>{`Time: ${player.time} sec`}</p>
                             </li>
                         )
                     })
